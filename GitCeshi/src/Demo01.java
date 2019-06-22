ab8459ab9183c6210fa1e3bee6ed96b97dd192f4
sfd <dfdfdfdf>
2019-06-22 19:29:58 +0800

    第一次提交

---
+++ GitCeshi/src/Demo01.java
@@ -0,0 +1,8 @@
+public class Demo01 {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("你好git");
+            i++;
+        }
+    }
+}
